532b44d94c6038ef684a141c25b2449cf5c3d78d
trickstival <trick_stival@hotmail.com>
2020-06-28 19:11:28 -0300

    feat(header): first look

---
+++ components/common/JbHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="jb-header">
+    <h1>
+      JUKEBOX
+    </h1>
+    <div class="separator"></div>
+    <h2>
+      Sala do choro
+    </h2>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.separator {
+  width: 2px;
+  height: 25px;
+  background-color: #fff;
+  margin: 0 15px;
+}
+
+.jb-header {
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  background-color: #1b1b1b;
+}
+</style>
